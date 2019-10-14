1df98175ebb513f27f137eeaaf6d5a30479c6495
cza <czasg0.0>
2019-10-14 16:16:26 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyws',
+    version='0.0.1',
+    description="This is a websocket-server by python",
+    author='czaOrz',
+    author_email='chenziangsg@163.com',
+    url='https://github.com/CzaOrz/Pyws',
+    packages=find_packages(),
+)
