b327e7fd4115a560af9c93a627a7060e0dfe473f
cza <czasg0.0>
2019-10-14 16:49:48 +0800

    rename pyws to pywss

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+#!/usr/bin/env python
+# coding:utf-8
 from setuptools import setup, find_packages
 
 setup(
-    name='pyws',
+    name='pywss',
     version='0.0.1',
-    description="This is a websocket-server by python",
+    description="This is a web-socket-server by python",
+    long_description="see https://github.com/CzaOrz/Pywss",
     author='czaOrz',
     author_email='chenziangsg@163.com',
-    url='https://github.com/CzaOrz/Pyws',
+    url='https://github.com/CzaOrz/Pywss',
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
 )
