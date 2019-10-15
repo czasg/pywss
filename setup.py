dba1aca17ad10b7ac5ba7a30fe2bb5282ebca17b
cza <czasg0.0>
2019-10-15 09:23:14 +0800

    0.0.2 is BUG, switch to 0.0.3

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pywss',
-    version='0.0.2',
+    version='0.0.3',
     description="This is a web-socket-server by python",
     long_description="see https://github.com/CzaOrz/Pywss",
     author='czaOrz',
     author_email='chenziangsg@163.com',
     url='https://github.com/CzaOrz/Pywss',
-    packages=find_packages('./pywss'),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
