9a91ffdb415667e02dd72b2e9f9b2ee3799a75ce
陈子昂 <chenziang@antiy.cn>
2022-09-23 17:02:03 +0800

    feat: 版本0.1.4

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -18,7 +18,7 @@ from pywss.closing import Closing
 from pywss.routing import Route
 from pywss.openapi import openapi_ui_template, merge_dict, parameters_filter
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 class Context:
