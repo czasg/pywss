953b69dd6fe5052ea229e660e369bad708b7a1f8
czasg <972542655@qq.com>
2022-10-07 16:58:57 +0800

    feat: 更新教程

--- bilibili/client.py
+++ bilibili/client.py
@@ -1,24 +1,8 @@
+# coding: utf-8
 import requests
 
-print(requests.post("http://localhost:8080/test", data="test").text)
+requests.post("http://localhost:8080/test", data="test")
 
-""" 演示框架 - hello world & 聊天室 & 在线协同编辑
-import pywss
-
-app = pywss.App()
-app.get("/", lambda ctx: ctx.write("hello world"))
-app.run()
-"""
-
-""" 搭架子
-1、搭建socket服务端，获取请求报文
-2、解析请求报文 method route version\r\nkey: value\r\n\r\ncontent
-- method, route, version, headers, content
-3、请求信息封装到context中
-4、路由模块
-5、http响应
-"""
-
-""" 如何深入?
-
+""" curl
+curl -X POST localhost:8080/test -d test
 """
