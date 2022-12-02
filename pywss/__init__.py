0ca6e2afc58b2bce941e4eae5f75cdcfa5e190a0
cza <44974561+czasg@users.noreply.github.com>
2022-12-02 14:31:02 +0800

    v0.1.13

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -19,7 +19,7 @@ from pywss.closing import Closing
 from pywss.routing import Route
 from pywss.openapi import openapi_ui_template, merge_dict, parameters_filter
 
-__version__ = '0.1.12'
+__version__ = '0.1.13'
 
 
 class Context:
