bc8ba55b78692f16d0bf67afdfc4c2627cef6f4a
陈子昂 <chenziang@antiy.cn>
2023-11-22 15:01:07 +0800

    v0.1.23

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -26,7 +26,7 @@ from pywss.routing import Route
 from pywss.openapi import openapi_ui_template
 from pywss.utils import split_method_route, merge_dict
 
-__version__ = '0.1.22'
+__version__ = '0.1.23'
 
 
 class Context:
