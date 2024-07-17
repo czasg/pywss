97a7bbff611230b7c465c5c14e351a16d3c1ff12
橙子昂 <44974561+czasg@users.noreply.github.com>
2024-07-17 10:40:26 +0800

    0.1.28

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -26,7 +26,7 @@ from pywss.routing import Route
 from pywss.openapi import openapi_ui_template
 from pywss.utils import split_method_route, merge_dict
 
-__version__ = '0.1.27'
+__version__ = '0.1.28'
 
 
 class Context:
