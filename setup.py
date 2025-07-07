fedf8d9ff51e1c442c4a875f8b3359d4bd22b4c0
czasg <972542655@qq.com>
2025-07-07 23:23:01 +0800

    0.2.1

--- setup.py
+++ setup.py
@@ -55,8 +55,7 @@ setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     install_requires=[
         "loggus>=0.1.1",
