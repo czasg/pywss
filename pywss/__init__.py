0cc6132398723df21c3d213867fc5d16d6a53545
橙子橙子 <44974561+czasg@users.noreply.github.com>
2025-07-11 10:39:26 +0800

    0.2.4

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -27,7 +27,7 @@ from pywss.routing import Route
 from pywss.openapi import openapi_ui_template
 from pywss.utils import split_method_route, merge_dict, Query
 
-__version__ = '0.2.3'
+__version__ = '0.2.4'
 
 
 class Context:
