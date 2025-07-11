9fff46aae4e0f91b7a4516c20f30483f2d3d8281
陈子昂 <chenziang1@xiaomi.com>
2025-07-11 10:04:26 +0800

    fix: 修复json长度计算异常

--- pywss/__init__.py
+++ pywss/__init__.py
@@ -317,6 +317,7 @@ class Context:
     def write_json(self, data, **kwargs) -> None:
         if not isinstance(data, str):
             data = json.dumps(data, ensure_ascii=False, **kwargs).encode()
+        data = data.encode()
         self.set_content_length(len(data))
         self.set_content_type("application/json")
         self.response_body.append(data)
